d1ffe9e64a8d8448581652ad79a7f1c9d2609022
lethang <grsven.01@gmail.com>
2016-04-03 20:48:59 +0700

    static pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "123456",
+             password_confirmation: "123456")
+
+5.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+category1 = Category.create(name: 'Basic 500 words')
+category2 = Category.create(name: 'Nature')
+category3 = Category.create(name: 'Animal')
+category4 = Category.create(name: 'Date')
+category5 = Category.create(name: 'Best Pet')
+
+30.times {
+  word_content = Faker::Lorem.word
+  category1.words.create!(content: word_content)
+}
+
+30.times {
+  word_content = Faker::Lorem.word 
+  category2.words.create!(content: word_content)
+}
+
+30.times {
+  word_content = Faker::Lorem.word 
+  category3.words.create!(content: word_content)
+}
+
+30.times {
+  word_content = Faker::Lorem.word 
+  category4.words.create!(content: word_content)
+}
+
