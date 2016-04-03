0d3df68fd97c1144e6cd184ca9f04e96a2e00fcb
lethang <grsven.01@gmail.com>
2016-04-03 21:00:20 +0700

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,5 @@
 Rails.application.routes.draw do
-
-  get 'sessions/new'
-
-  root             'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
-
-  resources :users do
-    resources :words
-    resources :lessons
-  end
-  
-  resources :categories
-  
-  namespace :admin do
-    DashboardManifest::DASHBOARDS.each do |dashboard_resource|
-      resources dashboard_resource
-    end
-
-    root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
-  end
-
+  root 'static_pages#home'
+  get  'static_pages/help'
+  get  'static_pages/about'
 end
